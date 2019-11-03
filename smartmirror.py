b9ed3a0bf13112be0abebd4280b9e3326afc2269
Austin Jones <austinjones@Austin-MBP.local>
2019-11-02 23:13:04 -0400

    first commit

--- smartmirror.py
+++ smartmirror.py
@@ -2,7 +2,7 @@
 # requirements
 # requests, feedparser, traceback, Pillow
 
-from Tkinter import *
+from tkinter import *
 import locale
 import threading
 import time
@@ -199,7 +199,7 @@ class Weather(Frame):
                     self.locationLbl.config(text=location2)
         except Exception as e:
             traceback.print_exc()
-            print "Error: %s. Cannot get weather." % e
+            print("Error: %s. Cannot get weather." % e)
 
         self.after(600000, self.get_weather)
 
@@ -236,7 +236,7 @@ class News(Frame):
                 headline.pack(side=TOP, anchor=W)
         except Exception as e:
             traceback.print_exc()
-            print "Error: %s. Cannot get news." % e
+            print("Error: %s. Cannot get news." % e)
 
         self.after(600000, self.get_headlines)
 
